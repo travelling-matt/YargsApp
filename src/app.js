aeef5717acb7a1fd605b4688fa4fc7060e6ae25c
Matt Burton <mattwburton@gmail.com>
2021-11-29 15:09:18 +0000

    add movie by title and actor

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const yargs = require("yargs");
+
+const { Movie, movieArr } = require("./utils");
+
+//'yargs' returns an object. 'process' returns and array. object is easier for us to work with.
+// console.log(yargs.argv);
+// console.log(process.argv);
+
+//const movie = new Movie(args.movie, args.actor);
+//'args.movie' and 'args.actor' replace 'args[3]' and 'args[4]'we used this morning
+
+const app = (args) => {
+    switch(process.argv[2]){
+        case "add":
+            const movie = new Movie(args.title, args.actor);
+            movie.add();
+            console.log(movieArr);
+            break;
+        case "add multi":
+            const movie1 = new Movie(args.title1, args.actor1);
+            const movie2 = new Movie(args.title2, args.actor2);
+            movie1.add();
+            movie2.add();
+            console.log(movieArr);
+            break;
+        default:
+            console.log("Incorrect command");
+            break;
+    }
+};
+
+app(yargs.argv);
+
+//args.name must match input name. put -- in front of name (see below)
+//node src/app.js "add" --title="spiderman" --actor="tom holland"
+//expected result
+//[ Movie { title: 'spiderman', actor: 'tom holland' } ]
---
+++ src/utils/index.js
@@ -0,0 +1,17 @@
+const movieArr = [];
+
+class Movie {
+    constructor(title, actor) {
+        this.title = title;
+        this.actor = actor;
+    }
+
+    add() {
+        movieArr.push(this);
+    }
+}
+
+module.exports = {
+    Movie,
+    movieArr,
+};
